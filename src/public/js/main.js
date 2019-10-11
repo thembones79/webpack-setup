ee1e71f77716aa2e1ff56c38bc46d10931151c35
thembones79 <michal.szumnarski@gmail.com>
2019-10-11 22:40:11 +0200

    it works, and it is super minimal

---
+++ src/public/js/main.js
@@ -0,0 +1,7 @@
+const React = require("react");
+const ReactDOM = require("react-dom")
+
+const App = () => <h1>Hello world6!</h1>;
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require("path");
 
-
 module.exports = {
-    entry: path.resolve(__dirname, "src", "public", "js", "main.js"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    mode: "development"
+  entry: path.resolve(__dirname, "src", "public", "js", "main.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: [
+          {
+            loader: "babel-loader",
+            query: {
+              presets: ["@babel/preset-react"]
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
